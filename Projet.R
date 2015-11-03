abc4141ebdfedd61b7d11d99b2ccec26367b6037
Brendan Le Lain <brendanlelain@hotmail.com>
2015-11-03 17:09:36 +0100

    elimination descripteurs redondants, 2 variables

--- Projet.R
+++ Projet.R
@@ -8,11 +8,9 @@ x <- read.table('FW_groupe5.txt',header=T,sep="\t", dec=",",row.names=1)
 data<-read.table("FW_groupe5.csv",sep=";",dec=",",header=TRUE,row.names=1) 
 attach(data)
 
-#voir pour eventuellement enlever la premiere colonne d
-
 N=length(data)
 
-#pour 1
+#pour 1 variable
 res=matrix(data=rep(0,N),nrow=N,ncol=N)
 for (i in 1:(N-1))
 {
@@ -22,26 +20,36 @@ for (i in 1:(N-1))
     res[i,j] = summary(modele)$r.squared
   }
 }
- 
-which(res>0.98,arr.ind=T)
-plot(data[[2]],data[[7]])
 
+redondant=unique(which(res>0.96,arr.ind=T)[,2])
+redondant
+data=data[,-redondant] #on enleve les descripteurs lies
+length(data)
 
-#avant de commencer, supprimer ceux de l'etape 1
 #pour 2
 N=length(data)
 res=array(data=rep(0,N),dim=c(N,N,N))
 #attention on a pas tout vu, par exemple X2~X1+X3, etc.
-for (i in 1:(N-2))
+J=1:(N-1)
+K=1:N
+compte=c()
+for (i in 1:N)
 {
-  for (j in (i+1):(N-1))
+  compte=c(compte,i)
+  for (j in J[-i])
   {
-    for (k in (j+1):N)
+    compte=c(compte,j)
+    for (k in K[-compte])
     {
       modele = lm(data[[i]] ~ data[[j]]+data[[k]], data=data)
       res[j,k,i] = summary(modele)$r.squared
     }
   }
+  compte=c()
 }
-which(res>0.95,arr.ind=T)
 
+redondant=unique(which(res>0.97,arr.ind=T)[,3])
+redondant
+
+data2=data[,-redondant]
+length(data2)
