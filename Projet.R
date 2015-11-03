2bdecac3635323549a3a80a66b15f71a8f4fdd73
Brendan Le Lain <brendanlelain@hotmail.com>
2015-11-03 17:45:18 +0100

    debut elimination 3 variables

--- Projet.R
+++ Projet.R
@@ -23,13 +23,13 @@ for (i in 1:(N-1))
 
 redondant=unique(which(res>0.96,arr.ind=T)[,2])
 redondant
-data=data[,-redondant] #on enleve les descripteurs lies
-length(data)
+data1=data[,-redondant] #on enleve les descripteurs lies
+length(data1)
 
 #pour 2
-N=length(data)
+N=length(data1)
 res=array(data=rep(0,N),dim=c(N,N,N))
-#attention on a pas tout vu, par exemple X2~X1+X3, etc.
+
 J=1:(N-1)
 K=1:N
 compte=c()
@@ -41,8 +41,8 @@ for (i in 1:N)
     compte=c(compte,j)
     for (k in K[-compte])
     {
-      modele = lm(data[[i]] ~ data[[j]]+data[[k]], data=data)
-      res[j,k,i] = summary(modele)$r.squared
+      modele = lm(data1[[i]] ~ data1[[j]]+data1[[k]], data=data1)
+      res[j,k,i] = summary(modele)$r.squared   #ou r ajuste??
     }
   }
   compte=c()
@@ -51,5 +51,49 @@ for (i in 1:N)
 redondant=unique(which(res>0.97,arr.ind=T)[,3])
 redondant
 
-data2=data[,-redondant]
+data2=data1[,-redondant]
 length(data2)
+
+
+#pour liaison entre 3 variables
+N=length(data2)
+N=6
+res=array(data=rep(0,N),dim=c(N,N,N,N))
+
+J=1:(N-1)
+K=1:(N-1)
+L=1:N
+compte_k=c()
+compte_l=c()
+for (i in 1:N)
+{
+  
+  compte_k=c(compte_k,i)
+  compte_l=c(compte_l,i)
+  for (j in J[-i])
+  {
+    
+    compte_k=c(compte_k,j)
+    compte_l=c(compte_l,j)
+    
+    for (k in K[-compte_k])
+    {
+      compte_l=c(compte_l,k)
+      print(L[-compte_l])   #pb ici, compte_l a revoir
+      for (l in L[-compte_l])
+      {
+        modele = lm(data2[[i]] ~ data2[[j]]+data2[[k]]+data2[[l]], data=data2)
+        res[j,k,l,i] = summary(modele)$r.squared   #ou r ajuste??
+      }
+    }
+  }
+  compte_k=c()
+  compte_l=c()
+}
+
+redondant=unique(which(res>0.97,arr.ind=T)[,3])
+redondant
+
+data2=data1[,-redondant]
+length(data2)
+
