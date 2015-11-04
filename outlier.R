8b415967d7951eca379cd66783bf856a52116125
Brendan Le Lain <brendanlelain@hotmail.com>
2015-11-04 13:08:43 +0100

    lecture traitement données aberrantes, petits essais

--- outlier.R
+++ outlier.R
@@ -3,3 +3,12 @@
 data<-read.table("FW_groupe5.csv",sep=";",dec=",",header=TRUE,row.names=1) 
 attach(data)
 N=length(data)
+Extreme=matrix(ncol=2,nrow=N)
+
+for (i in 1:N)
+{
+  Extreme[i,1]=which.min(data[[i]])
+  Extreme[i,2]=which.max(data[[i]])
+}
+unique(Extreme[,1])
+unique(Extreme[,2])
